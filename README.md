c32d486c3412f44d8a322dd57dbb4155f64df879
engsr6982 <pwxg286704@163.com>
2024-11-08 22:52:44 +0800

    完成

--- README.md
+++ README.md
@@ -1,35 +1,22 @@
-# LeviLamina Mod Template
+# CustomAttackRange
 
-A LeviLamina mod template
+自定义物品攻击距离
 
-This mod is a template for developing LeviLamina mods.
+## 安装 / Install
 
-## Install
+```bash
+lip install github.com/engsr6982/CustomAttackRange
+```
 
-Generate a new repository from this template.
+## 使用 / Usage
 
-## Usage
+- plugins/CustomAttackRange/config/Config.json
 
-Before using this mod template, make sure that you have installed XMake and a Minecraft Bedrock Server with LeviLamina.
-
-1. Clone the new repository into a local folder.
-
-1. Change the mod name and the expected LeviLamina version in `xmake.lua`.
-
-1. Add your code.
-
-1. Run `xmake repo -u` in the root of the repository.
-
-1. Run `xmake` to build the mod.
-
-Now the build is complete at `bin/`.
-
-## Contributing
-
-Ask questions by creating an issue.
-
-PRs accepted.
-
-## License
-
-CC0-1.0 © LiteLDev
+```json
+{
+  "version": 1,
+  "items": {
+    "minecraft:stick": 50.0 // key: Item type name, value: Attack range
+  }
+}
+```
--- src/mod/MyMod.cpp
+++ src/mod/MyMod.cpp
@@ -1,30 +1,106 @@
 #include "mod/MyMod.h"
 
 #include <memory>
+#include <string>
+#include <unordered_map>
 
+#include "ll/api/Config.h"
+#include "ll/api/event/EventBus.h"
+#include "ll/api/event/Listener.h"
+#include "ll/api/event/ListenerBase.h"
+#include "ll/api/event/player/PlayerClickEvent.h"
+#include "ll/api/memory/Hook.h"
 #include "ll/api/mod/RegisterHelper.h"
+#include "mc/network/ServerNetworkHandler.h"
+#include "mc/network/packet/PlayerActionPacket.h"
+#include "mc/world/actor/player/Player.h"
+#include "mc/world/item/registry/ItemStack.h"
+
 
 namespace my_mod {
 
-static std::unique_ptr<MyMod> instance;
+struct Config {
+    int                                     version{1};
+    std::unordered_map<std::string, double> items; // Example: {"item_name": 1}
 
-MyMod& MyMod::getInstance() { return *instance; }
+    static Config cfg;
+
+    static void load() {
+        bool ok = ll::config::loadConfig(Config::cfg, MyMod::getInstance().getSelf().getConfigDir() / "Config.json");
+        if (!ok) {
+            save();
+        }
+    }
+    static void save() {
+        ll::config::saveConfig(Config::cfg, MyMod::getInstance().getSelf().getConfigDir() / "Config.json");
+    }
+};
+Config Config::cfg;
+
+
+LL_TYPE_INSTANCE_HOOK(
+    HandlerPlayerActionPacketHook,
+    ll::memory::HookPriority::Normal,
+    ServerNetworkHandler,
+    &ServerNetworkHandler::handle,
+    void,
+    NetworkIdentifier const&  source,
+    PlayerActionPacket const& packet
+) {
+    if (auto player = getServerPlayer(source, packet.mClientSubId)) {
+        Player&          pl = *player;
+        ItemStack const& it = pl.getSelectedItem();
+
+        if (Config::cfg.items.contains(it.getTypeName())) {
+            auto const& dt = Config::cfg.items[it.getTypeName()];
+            if (dt < 0) {
+                return;
+            }
+
+            HitResult val = pl.traceRay(static_cast<float>(dt), true, false);
+            Actor*    act = val.getEntity();
+            if (!act) {
+                return;
+            }
+
+            pl.attack(*act, ::ActorDamageCause::EntityAttack);
+        }
+    }
+    origin(source, packet);
+}
+
+
+static std::unique_ptr<MyMod> instance;
+MyMod&                        MyMod::getInstance() { return *instance; }
 
 bool MyMod::load() {
     getSelf().getLogger().debug("Loading...");
-    // Code for loading the mod goes here.
+    getSelf().getLogger().info("Author: engsr6982");
+    getSelf().getLogger().info("LICENSE: GPLv3");
+
+    Config::load();
+
     return true;
 }
 
 bool MyMod::enable() {
     getSelf().getLogger().debug("Enabling...");
-    // Code for enabling the mod goes here.
+
+    HandlerPlayerActionPacketHook::hook();
+
     return true;
 }
 
 bool MyMod::disable() {
     getSelf().getLogger().debug("Disabling...");
-    // Code for disabling the mod goes here.
+
+    HandlerPlayerActionPacketHook::unhook();
+
+    return true;
+}
+
+bool MyMod::unload() {
+    getSelf().getLogger().debug("Unloading...");
     return true;
 }
 
